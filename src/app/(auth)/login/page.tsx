98dcb9ee22924757f8ac4f6154a36606e0996716
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-02-03 00:38:32 +0700

    Add Login components and layout for user authentication

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginContainer from "@/features/auth/login/container/LoginContainer";
+
+const page = () => {
+  return (
+    <div className="centered justify-center">
+      <LoginContainer />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/features/auth/login/components/LoginSection.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const LoginSection = () => {
+  return (
+    <div className="bg-card relative mx-auto w-full max-w-[50%] overflow-hidden rounded-4xl border shadow-2xl">
+      <div className="p-8">
+        <h1 className="title text-center text-2xl">Welcome To Merx</h1>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSection;
