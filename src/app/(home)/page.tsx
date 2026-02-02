e0dedf6e4b11ff3d0b7cc271df48f42ad7460fbb
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-02-02 13:29:12 +0700

    Refactor global styles and add Hero components for improved layout and design

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import HeroContainer from "@/features/home/hero-section/container/HeroContainer";
+
+const page = () => {
+  return (
+    <>
+      <main>
+        <HeroContainer />
+      </main>
+    </>
+  );
+};
+
+export default page;
---
+++ src/features/home/hero-section/container/HeroContainer.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "@/features/home/hero-section/components/HeroSection";
+
+const HeroContainer = () => {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+};
+
+export default HeroContainer;
