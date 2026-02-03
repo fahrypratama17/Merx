d4f5a38c9df089a1051cde062087a9b741d05f97
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-02-03 21:48:45 +0700

    Add AnimatedProducts component for showcasing testimonials with animations

--- src/shared/components/Card.tsx
+++ src/shared/components/Card.tsx
@@ -2,7 +2,7 @@ import { cardProps } from "@/shared/types/types";
 
 const Card = ({ icon, title, sub }: cardProps) => {
   return (
-    <div className="bg-muted/40 border-border/60 rounded-xl border p-4 shadow-lg transition-colors duration-300 hover:border-black hover:shadow-2xl">
+    <div className="bg-card border-border/60 rounded-xl border p-4 shadow-lg transition-colors duration-300 hover:border-black hover:shadow-2xl">
       <img src={icon} alt={"icon"} width={28} height={28} className="mb-2" />
       <h3 className="mb-2 text-lg font-medium">{title}</h3>
       <p className="mt-4 text-sm leading-relaxed">{sub}</p>
